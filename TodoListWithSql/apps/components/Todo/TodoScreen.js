ffaf1e78a36c4bca621f4760f59d6b6a545496a0
sinbbgirl <66307868+sinbbgirl@users.noreply.github.com>
2021-02-07 03:54:32 +0900

    -

--- TodoListWithSql/apps/components/Todo/TodoScreen.js
+++ TodoListWithSql/apps/components/Todo/TodoScreen.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
-import React, {Component, useEffect} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {View} from 'react-native';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
 import {openDatabase} from 'react-native-sqlite-storage';
